bf7d279e80f1facc4f337a8cdf64ecd08f6f299b
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-08-11 19:36:04 +0300

    black formatting

--- speech_detection.py
+++ speech_detection.py
@@ -1,34 +1,38 @@
-import numpy as np
 from math import ceil
+
+import numpy as np
 from scipy.signal import medfilt
 
+
 def signal_energy(sound, window_length):
-    
+
     num_frames = ceil(sound.size / window_length) if window_length > 0 else 0
     result = np.empty(num_frames)
-    
+
     max = np.max(sound)
-    if max > 0: sound = sound / max
-    
+    if max > 0:
+        sound = sound / max
+
     pos = 0
     for i in range(num_frames):
         next_pos = pos + window_length
-        window = sound[pos:(next_pos if next_pos <= sound.size else sound.size)]
+        window = sound[pos : (next_pos if next_pos <= sound.size else sound.size)]
         result[i] = 1 / window.size * np.sum(np.square(window))
         pos = next_pos
-        
+
     return result
 
+
 def spectral_centroid(sound, window_length):
-    
+
     num_frames = ceil(sound.size / window_length) if window_length > 0 else 0
     result = np.empty(num_frames)
-    
+
     pos = 0
     for i in range(num_frames):
         next_pos = pos + window_length
-        window = sound[pos:(next_pos if next_pos <= sound.size else sound.size)]
-        
+        window = sound[pos : (next_pos if next_pos <= sound.size else sound.size)]
+
         fft = np.fft.rfft(window)
         sum = 0
         freq_sum = 0
@@ -36,59 +40,64 @@ def spectral_centroid(sound, window_length):
             sum += np.abs(fft[j])
             freq_sum += (j + 1) * np.abs(fft[j])
         result[i] = (freq_sum / sum) if sum != 0 else 0
-        
+
         pos = next_pos
-    
+
     return result
+
 
 def smooth_signal(signal, window_length):
     result = medfilt(signal, window_length)
     result = medfilt(result, window_length)
     return result
 
+
 def max_values(signal, num_values):
-    
+
     max = [0] * num_values
-    hist = np.histogram(signal, bins = 'auto')
-    
+    hist = np.histogram(signal, bins="auto")
+
     for i in range(hist[0].size):
         maxv = [hist[0][j] for j in max]
         if hist[0][i] > np.min(maxv):
             max[np.argmin(maxv)] = i
-    
+
     return [(hist[1][i] + hist[1][i + 1]) * 0.5 for i in max]
+
 
 def calculate_mask(signal, max, weight):
     th = (weight * max[0] + max[1]) / (weight + 1)
     return [1 if y > th else 0 for y in signal]
 
+
 def post_process(mask, sound_length, window_length, extend_length):
-    
+
     res = [0] * sound_length
-    
+
     def set(start, end):
         start = start if start >= 0 else 0
         end = end if end <= sound_length else sound_length
         for i in range(start, end):
             res[i] = 1
-    
+
     pos = 0
     for i in range(mask.size):
         next_pos = pos + window_length
-        if (mask[i] > 0):
+        if mask[i] > 0:
             set(pos, next_pos)
         if i > 0 and mask[i] > 0 and mask[i - 1] == 0:
             set(pos - extend_length, pos)
         if i < mask.size and mask[i] > 0 and mask[i + 1] == 0:
             set(next_pos + 1, next_pos + extend_length)
         pos = next_pos
-        
+
     return res
 
-def detect_speech(sound, df, draw = 0):
-    
+
+def detect_speech(sound, df, draw=0):
+
     window_length = round(0.025 * df)
-    
+
     nrg = signal_energy(sound, window_length)
     spc = spectral_centroid(sound, window_length)
     nrgsm = smooth_signal(nrg, 5)
@@ -98,11 +107,11 @@ def detect_speech(sound, df, draw = 0):
     nrgmsk = calculate_mask(nrg, maxnrg, 5.0)
     spcmsk = calculate_mask(spc, maxspc, 5.0)
     mask = post_process(np.multiply(nrgmsk, spcmsk), sound.size, window_length, 5 * window_length)
-    
+
     if draw:
         import matplotlib.pyplot as plt
         from librosa import display
-        
+
         fig, axs = plt.subplots(2, 3)
         display.waveshow(sound, ax=axs[0][0])
         axs[1][0].plot(mask)
@@ -111,8 +120,9 @@ def detect_speech(sound, df, draw = 0):
         axs[0][2].plot(spc)
         axs[1][2].plot(spcsm)
         plt.show()
-        
+
     return mask
+
 
 def remove_silence(sound, df):
     mask = detect_speech(sound, df)
