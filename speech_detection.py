maximas calculation fixes

speech_detection.py
@@ -2,43 +2,41 @@ import numpy as np
 from math import ceil
 from scipy.signal import medfilt
 
-def signal_energy(sound, window_length):
-    
-    num_frames = ceil(sound.size / window_length) if window_length > 0 else 0
+def normalize(sound: np.ndarray) -> np.ndarray:
+    max = sound.max()
+    return sound / max if max > 0 else sound
+
+def signal_energy(sound, window_length_samp):
+    assert window_length_samp > 0, "Window length must be positive integer"
+    sound = normalize(sound)
+
+    num_frames = ceil(len(sound) / window_length_samp)
     result = np.empty(num_frames)
-    
-    max = np.max(sound)
-    if max > 0: sound = sound / max
-    
-    pos = 0
+    lo, hi = 0, window_length_samp
     for i in range(num_frames):
-        next_pos = pos + window_length
-        window = sound[pos:(next_pos if next_pos <= sound.size else sound.size)]
-        result[i] = 1 / window.size * np.sum(np.square(window))
-        pos = next_pos
-        
+        result[i] = np.square(sound[lo:hi]).mean()
+        lo += window_length_samp
+        hi += window_length_samp
+
     return result
 
-def spectral_centroid(sound, window_length):
-    
-    num_frames = ceil(sound.size / window_length) if window_length > 0 else 0
+def spectral_centroid(sound, window_length_samp):
+    assert window_length_samp > 0, "Window length must be positive integer"
+    num_frames = ceil(len(sound) / window_length_samp)
     result = np.empty(num_frames)
-    
-    pos = 0
+
+    lo, hi = 0, window_length_samp
     for i in range(num_frames):
-        next_pos = pos + window_length
-        window = sound[pos:(next_pos if next_pos <= sound.size else sound.size)]
-        
+        window = sound[lo:hi]
+        window = np.multiply(window, np.hamming(len(window)))
         fft = np.fft.rfft(window)
-        sum = 0
-        freq_sum = 0
-        for j in range(fft.size):
-            sum += np.abs(fft[j])
-            freq_sum += (j + 1) * np.abs(fft[j])
-        result[i] = (freq_sum / sum) if sum != 0 else 0
-        
-        pos = next_pos
-    
+        freq_sum = sum((j + 1) * np.abs(f) for j, f in enumerate(fft))
+        fft_sum = np.abs(fft).sum()
+        result[i] = freq_sum / fft_sum if fft_sum else 0
+
+        lo += window_length_samp
+        hi += window_length_samp
+
     return result
 
 def smooth_signal(signal, window_length):
@@ -46,24 +44,29 @@ def smooth_signal(signal, window_length):
     result = medfilt(result, window_length)
     return result
 
-def max_values(signal, num_values):
+def max_values(hist, num_values):
+    max_args = [-1] * num_values
+    bin_counts, bin_edges = hist[0], hist[1]
+    bound = 0.02 * np.mean(bin_counts)
     
-    max = [0] * num_values
-    hist = np.histogram(signal, bins = 'auto')
+    i = 0
+    while i < len(bin_counts):
+        max_vals = [bin_counts[j] if j >= 0 else 0 for j in max_args]
+        if bin_counts[i] > np.min(max_vals) and (i == 0 or bin_counts[i] > bin_counts[i - 1]) and (i == (len(bin_counts) - 1) or bin_counts[i] > bin_counts[i + 1]) and bin_counts[i] > bound:
+            max_args[np.argmin(max_vals)] = i
+            i += 2
+        else: i += 1
+        
+    max_args = np.sort(max_args)
     
-    for i in range(hist[0].size):
-        maxv = [hist[0][j] for j in max]
-        if hist[0][i] > np.min(maxv):
-            max[np.argmin(maxv)] = i
-    
-    return [(hist[1][i] + hist[1][i + 1]) * 0.5 for i in max]
+    return np.array([((bin_edges[i] + bin_edges[i + 1]) * 0.5) if i >= 0 else -1.0 for i in max_args])
 
-def calculate_mask(signal, max, weight):
-    th = (weight * max[0] + max[1]) / (weight + 1)
-    return [1 if y > th else 0 for y in signal]
+def calculate_threshold(signal, max, weight):
+    if np.count_nonzero(max >= 0.0) < 2:
+        return np.mean(signal)
+    return (weight * max[0] + max[1]) / (weight + 1)
 
 def post_process(mask, sound_length, window_length, extend_length):
-    
     res = [0] * sound_length
     
     def set(start, end):
@@ -73,47 +76,61 @@ def post_process(mask, sound_length, window_length, extend_length):
             res[i] = 1
     
     pos = 0
-    for i in range(mask.size):
+    for i in range(len(mask)):
         next_pos = pos + window_length
-        if (mask[i] > 0):
+        if (mask[i]):
             set(pos, next_pos)
-        if i > 0 and mask[i] > 0 and mask[i - 1] == 0:
+        if i > 0 and mask[i] and not mask[i - 1]:
             set(pos - extend_length, pos)
-        if i < mask.size and mask[i] > 0 and mask[i + 1] == 0:
+        if i < (len(mask) - 1) and mask[i] and not mask[i + 1]:
             set(next_pos + 1, next_pos + extend_length)
         pos = next_pos
         
     return res
 
-def detect_speech(sound, df, draw = 0):
-    
-    window_length = round(0.025 * df)
+def detect_speech(sound, sr, draw = False):
+    window_length = round(0.025 * sr)
+    num_bins = round(0.002 * sr)
     
     nrg = signal_energy(sound, window_length)
     spc = spectral_centroid(sound, window_length)
     nrgsm = smooth_signal(nrg, 5)
     spcsm = smooth_signal(spc, 5)
-    maxnrg = max_values(nrgsm, 2)
-    maxspc = max_values(spcsm, 2)
-    nrgmsk = calculate_mask(nrg, maxnrg, 5.0)
-    spcmsk = calculate_mask(spc, maxspc, 5.0)
-    mask = post_process(np.multiply(nrgmsk, spcmsk), sound.size, window_length, 5 * window_length)
+    nrghst = np.histogram(nrg, num_bins)
+    spchst = np.histogram(spc, num_bins)
+    maxnrg = max_values(nrghst, 2)
+    maxspc = max_values(spchst, 2)
+    nrgth = calculate_threshold(nrg, maxnrg, 5.0)
+    spcth = calculate_threshold(spc, maxspc, 5.0)
+    nrgmsk = nrg > nrgth
+    spcmsk = spc > spcth
+    mask = post_process(np.logical_and(nrgmsk, spcmsk), len(sound), window_length, 5 * window_length)
+    
+    print(maxnrg)
+    print(nrgth)
+    print(maxspc)
+    print(spcth)
     
     if draw:
         import matplotlib.pyplot as plt
-        from librosa import display
         
         fig, axs = plt.subplots(2, 3)
-        display.waveshow(sound, ax=axs[0][0])
-        axs[1][0].plot(mask)
-        axs[0][1].plot(nrg)
-        axs[1][1].plot(nrgsm)
-        axs[0][2].plot(spc)
-        axs[1][2].plot(spcsm)
+        axs[0][0].plot(sound, linewidth = 0.5)
+        axs[1][0].plot(mask, linewidth = 0.7, zorder = 2)
+        axs[1][0].plot(nrgmsk, linewidth = 0.4, zorder = 1)
+        axs[1][0].plot(spcmsk, linewidth = 0.4, zorder = 1)
+        axs[0][1].plot(nrg, linewidth = 0.5, zorder = 1)
+        axs[0][1].plot(nrgsm, linewidth = 0.5, zorder = 2)
+        axs[0][1].plot([0, len(nrg)], [nrgth, nrgth], linewidth = 0.7, zorder = 3)
+        axs[1][1].plot(spc, linewidth = 0.5, zorder = 1)
+        axs[1][1].plot(spcsm, linewidth = 0.5, zorder = 2)
+        axs[1][1].plot([0, len(spc)], [spcth, spcth], linewidth = 0.7, zorder = 3)
+        axs[0][2].bar(nrghst[1][:-1], nrghst[0], width=np.diff(nrghst[1]))
+        axs[1][2].bar(spchst[1][:-1], spchst[0], width=np.diff(spchst[1]))
         plt.show()
         
     return mask
 
 def remove_silence(sound, df):
     mask = detect_speech(sound, df)
-    return np.multiply(sound, mask)
+    return np.multiply(sound, mask).astype(np.int16)