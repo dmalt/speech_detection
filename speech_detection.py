04efe777198f7362744f370d0941f8bed0e2fb48
mr7falcon <mr.falcon.inc@gmail.com>
2022-08-23 23:39:28 +0300

    1. Use Freedman-Diaconis rule to calculate the number of bins of the histogram; 2. Use spectral spread instead of spectral centroid; 3. Calculate correlation between features to know where we shound thresholding to.

--- speech_detection.py
+++ speech_detection.py
@@ -1,6 +1,7 @@
+from functools import cmp_to_key
 import numpy as np
-from math import ceil
-from scipy.signal import medfilt
+from math import ceil, sqrt
+from scipy.signal import medfilt, butter, lfilter
 
 def normalize(sound: np.ndarray) -> np.ndarray:
     max = sound.max()
@@ -29,11 +30,41 @@ def spectral_centroid(sound, window_length_samp):
     for i in range(num_frames):
         window = sound[lo:hi]
         window = np.multiply(window, np.hamming(len(window)))
-        fft = np.fft.rfft(window)
-        freq_sum = sum((j + 1) * np.abs(f) for j, f in enumerate(fft))
-        fft_sum = np.abs(fft).sum()
+        
+        fft = np.abs(np.fft.rfft(window))
+        fft = np.multiply(fft, fft >= 0.002 * window_length_samp)
+        fft = normalize(fft)
+        
+        freq_sum = sum((j + 1) * f for j, f in enumerate(fft))
+        fft_sum = np.sum(fft)
         result[i] = freq_sum / fft_sum if fft_sum else 0
+        
+        lo += window_length_samp
+        hi += window_length_samp
 
+    return result
+
+def spectral_spread(sound, window_length_samp):
+    assert window_length_samp > 0, "Window length must be positive integer"
+    num_frames = ceil(len(sound) / window_length_samp)
+    result = np.empty(num_frames)
+
+    lo, hi = 0, window_length_samp
+    for i in range(num_frames):
+        window = sound[lo:hi]
+        window = np.multiply(window, np.hamming(len(window)))
+        
+        fft = np.abs(np.fft.rfft(window))
+        fft = np.multiply(fft, fft >= 0.002 * window_length_samp)
+        fft = normalize(fft)
+        
+        freq_sum = sum((f + 1) * s for f, s in enumerate(fft))
+        fft_sum = np.sum(fft)
+        centroid = freq_sum / fft_sum if fft_sum else 0
+        
+        freq_sum = sum((f + 1 - centroid) * (f + 1 - centroid) * s for f, s in enumerate(fft))
+        result[i] = sqrt(freq_sum / fft_sum if fft_sum else 0)
+        
         lo += window_length_samp
         hi += window_length_samp
 
@@ -57,53 +88,54 @@ def max_values(hist, num_values):
             i += 2
         else: i += 1
         
-    max_args = np.sort(max_args)
+    max_args = sorted(max_args)
     
-    return np.array([((bin_edges[i] + bin_edges[i + 1]) * 0.5) if i >= 0 else -1.0 for i in max_args])
+    return np.array([((bin_edges[i] + bin_edges[i + 1]) * 0.5) if i >= 0 else np.nan for i in max_args])
 
 def calculate_threshold(signal, max, weight):
-    if np.count_nonzero(max >= 0.0) < 2:
+    if np.count_nonzero(~np.isnan(max)) < 2:
         return np.mean(signal)
     return (weight * max[0] + max[1]) / (weight + 1)
 
+def calculate_mask(signal, max, th):
+    return signal > th if max[0] <= max[1] else signal < th
+
 def post_process(mask, sound_length, window_length, extend_length):
-    res = [0] * sound_length
-    
-    def set(start, end):
-        start = start if start >= 0 else 0
-        end = end if end <= sound_length else sound_length
-        for i in range(start, end):
-            res[i] = 1
-    
+    res = np.zeros(sound_length).astype(bool)
+
     pos = 0
     for i in range(len(mask)):
         next_pos = pos + window_length
-        if (mask[i]):
-            set(pos, next_pos)
+        if mask[i]:
+            res[pos:next_pos] = True
         if i > 0 and mask[i] and not mask[i - 1]:
-            set(pos - extend_length, pos)
+            res[max(pos - extend_length, 0) : pos] = True
         if i < (len(mask) - 1) and mask[i] and not mask[i + 1]:
-            set(next_pos + 1, next_pos + extend_length)
+            res[next_pos : next_pos + extend_length] = True
         pos = next_pos
-        
+
     return res
 
 def detect_speech(sound, sr, draw = False):
-    window_length = round(0.025 * sr)
-    num_bins = round(0.002 * sr)
+    assert sr > 0, "Sample rate must be positive integer"
+    window_length = round(0.05 * sr)
+    thresholding_weight = 5.0
+    sm_filter_order = 5
     
     nrg = signal_energy(sound, window_length)
-    spc = spectral_centroid(sound, window_length)
-    nrgsm = smooth_signal(nrg, 5)
-    spcsm = smooth_signal(spc, 5)
-    nrghst = np.histogram(nrg, num_bins)
-    spchst = np.histogram(spc, num_bins)
+    spc = spectral_spread(sound, window_length)
+    nrgsm = smooth_signal(nrg, sm_filter_order)
+    spcsm = smooth_signal(spc, sm_filter_order)
+    nrghst = np.histogram(np.trim_zeros(nrgsm), 'fd')
+    spchst = np.histogram(np.trim_zeros(spcsm), 'fd')
     maxnrg = max_values(nrghst, 2)
     maxspc = max_values(spchst, 2)
-    nrgth = calculate_threshold(nrg, maxnrg, 5.0)
-    spcth = calculate_threshold(spc, maxspc, 5.0)
-    nrgmsk = nrg > nrgth
-    spcmsk = spc > spcth
+    if np.corrcoef(nrgsm, spcsm)[0][1] < 0:
+        maxspc = np.flip(maxspc)
+    nrgth = calculate_threshold(nrgsm, maxnrg, thresholding_weight)
+    spcth = calculate_threshold(spcsm, maxspc, thresholding_weight)
+    nrgmsk = calculate_mask(nrgsm, maxnrg, nrgth)
+    spcmsk = calculate_mask(spcsm, maxspc, spcth)
     mask = post_process(np.logical_and(nrgmsk, spcmsk), len(sound), window_length, 5 * window_length)
     
     print(maxnrg)
@@ -116,9 +148,8 @@ def detect_speech(sound, sr, draw = False):
         
         fig, axs = plt.subplots(2, 3)
         axs[0][0].plot(sound, linewidth = 0.5)
-        axs[1][0].plot(mask, linewidth = 0.7, zorder = 2)
-        axs[1][0].plot(nrgmsk, linewidth = 0.4, zorder = 1)
-        axs[1][0].plot(spcmsk, linewidth = 0.4, zorder = 1)
+        axs[0][0].plot(np.multiply(sound, mask), linewidth = 0.5)
+        axs[1][0].plot(mask)
         axs[0][1].plot(nrg, linewidth = 0.5, zorder = 1)
         axs[0][1].plot(nrgsm, linewidth = 0.5, zorder = 2)
         axs[0][1].plot([0, len(nrg)], [nrgth, nrgth], linewidth = 0.7, zorder = 3)
