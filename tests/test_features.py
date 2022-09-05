6341134284eba7526c80653930fd0295cb6dcd43
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-09-05 13:45:52 +0300

    test energy on single-value input

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,7 +1,14 @@
 import numpy as np
+from hypothesis import given
+from hypothesis.strategies import floats
 
 from speech_detection import features
 
 
 def test_energy_on_empty_input_returns_zero():
     assert features.compute_energy(np.array([])) == 0
+
+
+@given(floats(min_value=-1e12, max_value=1e12))
+def test_energy_on_single_value_returns_this_value_squared(val: float) -> None:
+    assert features.compute_energy(np.array([val])) == val ** 2
