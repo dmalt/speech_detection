20ab9e68bdcf834595754847b2e37a71592ee185
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-09-05 13:58:23 +0300

    test energy on repeated values

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,6 +1,7 @@
 import numpy as np
 from hypothesis import given
-from hypothesis.strategies import floats
+from hypothesis.strategies import floats, integers
+from numpy.testing import assert_almost_equal
 
 from speech_detection import features
 
@@ -12,3 +13,8 @@ def test_energy_on_empty_input_returns_zero():
 @given(floats(min_value=-1e12, max_value=1e12))
 def test_energy_on_single_value_returns_this_value_squared(val: float) -> None:
     assert features.compute_energy(np.array([val])) == val ** 2
+
+
+@given(floats(min_value=-1e6, max_value=1e6), integers(min_value=1, max_value=1_000_000))
+def test_energy_on_repeated_value_returns_this_value_squared(val: float, n: int) -> None:
+    assert_almost_equal(features.compute_energy(np.array([val] * n)), val ** 2, decimal=0)
